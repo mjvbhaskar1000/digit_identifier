4e71b10f731615f91fd993c15b6ec990c40e03d4
mjvbhaskar1000 <mjvbhaskar1000@gmail.com>
2016-08-21 03:13:18 -0500

    Update sklearnSVM.py

--- sklearnSVM.py
+++ sklearnSVM.py
@@ -1,9 +1,18 @@
+#importing the required modules.
 import sklearnMLCommon as cm
 from sklearn.svm import LinearSVC
 
+#http://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html#sklearn.svm.LinearSVC
+#building a model.
 svmc = LinearSVC()
+
+#training the model.
 svmc.fit(cm.xtrain, cm.ytrain)
+
+#testing the model.
 predictedVal = svmc.predict(cm.xtest)
+
+#calculating the accuracy and printing it.
 accuracy = cm.accuracy_score(cm.ytest, predictedVal)
 print "Accuracy: ", accuracy
 
