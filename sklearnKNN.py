45f0a033ea0c851cb19608e7ba53aaad6632acbb
mjvbhaskar1000 <mjvbhaskar1000@gmail.com>
2016-08-21 02:59:57 -0500

    Update sklearnKNN.py

--- sklearnKNN.py
+++ sklearnKNN.py
@@ -1,8 +1,18 @@
+#importing the required modules.
 import sklearnMLCommon as cm
 from sklearn.neighbors import KNeighborsClassifier
 
+#http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html#sklearn.neighbors.KNeighborsClassifier
+#defining a KNeighborsClassifier with n_jobs = 4. 
+#value of n_jobs much be modified depending the number of cores.
 knnc = KNeighborsClassifier(n_jobs=4)
+
+#training the model.
 knnc.fit(cm.xtrain, cm.ytrain)
+
+#testing the model.
 predictedVal = knnc.predict(cm.xtest)
+
+#calculating the accuracy and printing it.
 accuracy = cm.accuracy_score(cm.ytest, predictedVal)
 print "accuracy: ", accuracy
