83d6d641c0996f7f4c2e1ed8bb52e87f5ebd4811
mjvbhaskar1000 <mjvbhaskar1000@gmail.com>
2016-08-21 03:06:35 -0500

    Update sklearnRandomForest.py

--- sklearnRandomForest.py
+++ sklearnRandomForest.py
@@ -1,11 +1,17 @@
+#importing the required modules.
 import sklearnMLCommon as cm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.externals import joblib
 
+#Building the RandomForestClassifier with 200 trees and 4 parallel runs. 
 rfc = RandomForestClassifier(n_estimators=200, n_jobs = 4)
+
+#training the model.
 rfc.fit(cm.xtrain, cm.ytrain)
+
+#testing the model.
 predictedVal = rfc.predict(cm.xtest)
+
+#calculating and printing the accuracy of the model.
 accuracy = cm.accuracy_score(cm.ytest, predictedVal)
-#joblib.dump(rfc, 'rfc.pkl')
-#print rfc.feature_importances_
 print "Accuracy is: ", accuracy
